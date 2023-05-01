b4831c4e20f8a0f4c41164f673c013e27b944261
Luke Teo <yipengteo.luke@gmail.com>
2023-05-01 21:29:33 +0900

    feat: changed colorscheme and alpha

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,8 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(
+          bufnr) end)
       end,
       desc = "Pick to close",
     },
@@ -20,6 +21,14 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>c"] = {
+      function()
+        local bufs = vim.fn.getbufinfo { buflisted = true }
+        require("astronvim.utils.buffer").close(0)
+        if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+      end,
+      desc = "Close buffer",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
