0b2d6e218e7cf546a6575385f8d135ee11e07762
Luke Teo <yipengteo.luke@gmail.com>
2023-05-03 20:37:21 +0900

    feat: added new plugins, enable transparent window

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,8 +6,43 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- Color
+  { import = "astrocommunity.color.tint-nvim" },
+
+  -- ColorScheme
   { import = "astrocommunity.colorscheme.oxocarbon" },
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.colorscheme.poimandres" },
+
+  -- Diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  -- Editing Support
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+
+  -- Git
+  { import = "astrocommunity.git.git-blame-nvim" },
+
+  -- Indent
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+
+  -- Markdown and Latex
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  -- Language Packs
+  -- { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.php" },
+  -- { import = "astrocommunity.pack.svelte" },
+  -- { import = "astrocommunity.pack.tailwindcss" },
+  -- { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- { import = "astrocommunity.pack.yaml" },
+
+  -- Syntax
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+
+  -- Utility
+  { import = "astrocommunity.utility.transparent-nvim" },
+  { import = "astrocommunity.utility.neodim" },
 }
