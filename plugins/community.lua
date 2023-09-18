0052e6381669ad2d828b90eda2839cf6c75e9b67
luke-teo <yipengteo.luke@gmail.com>
2023-09-18 22:17:50 +0900

    fix: changed telescope settings, removed unnecessary packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,22 +2,11 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
 
-  -- Packs
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.svelte" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
-
   -- Color
   { import = "astrocommunity.color.tint-nvim" },
 
   -- ColorScheme
-  { import = "astrocommunity.colorscheme.nightfox-nvim" },
-  { import = "astrocommunity.colorscheme.mini-base16" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
 
   -- Diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim" },
@@ -36,15 +25,9 @@ return {
   { import = "astrocommunity.lsp.inc-rename-nvim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
 
-  -- Markdown and Latex
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-
   -- Motion
   { import = "astrocommunity.motion.hop-nvim" },
 
   -- Syntax
   { import = "astrocommunity.syntax.hlargs-nvim" },
-
-  -- Utility
-  { import = "astrocommunity.utility.transparent-nvim" },
 }
